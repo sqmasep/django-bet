fce024a761c2a37ea69910291a3e3572c96dd28b
Enzo <enzobouchez434@gmail.com>
2024-04-24 11:16:53 +0200

    [FEAT] api routes: addded the 'bets' test route

--- backend/core/bet/views.py
+++ backend/core/bet/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from.serializers import BetSerializer
+from.models import Bet
 
 # Create your views here.
+
+
+class BetViewSet(viewsets.ModelViewSet):
+    serializer_class = BetSerializer
+    queryset = Bet.objects.all()
