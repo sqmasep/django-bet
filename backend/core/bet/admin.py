131bb192896e38d21568cde6a7f4814fb631d1ab
Enzo <enzobouchez434@gmail.com>
2024-04-24 11:08:07 +0200

    [FEAT] added bet model in admin panel

--- backend/core/bet/admin.py
+++ backend/core/bet/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Bet
 # Register your models here.
+
+class BetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bet_amount')
+    
+admin.site.register(Bet)
