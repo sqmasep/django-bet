cce37a2942075a6572664d0b5d3755f4faa7528e
Enzo <enzobouchez434@gmail.com>
2024-04-26 15:53:10 +0200

    [FIX] return id in /me

--- backend/core/authentication/models.py
+++ backend/core/authentication/models.py
@@ -1,4 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import MinValueValidator
+from decimal import Decimal
+
 class CustomUser(AbstractUser):
-    fav_color = models.CharField(blank=True, max_length=120)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), validators=[MinValueValidator(Decimal('0.00'))])
+
+    def __str__(self):
+        return self.username
