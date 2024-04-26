eb59629b12c640b0b9d5841f9c57a1fdf646edf9
Enzo <enzobouchez434@gmail.com>
2024-04-26 12:22:40 +0200

    [FEAT] BET model with options

--- backend/core/bet/models.py
+++ backend/core/bet/models.py
@@ -4,10 +4,11 @@ from authentication.models import CustomUser
 # Create your models here.
 class Bet(models.Model):
     name = models.CharField(max_length = 100)
-    author = models.ForeignKey(CustomUser, on_delete = models.CASCADE)
+    author = models.ForeignKey(CustomUser, related_name='author', null=True, on_delete = models.CASCADE)
     
     def __str__(self):
         return self.name
     
-
-    
+class Option(models.Model):
+    text = models.CharField(max_length = 100)
+    bet = models.ForeignKey(Bet, related_name='options', on_delete = models.CASCADE)
